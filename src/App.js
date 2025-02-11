afe74bbb1e0373d6c4446d6bcd0b9d809b1bbb6d
FaridaAdamuMagaji <faridaadamumagaji@gmail.com>
2025-02-11 14:44:43 -0800

    added css files

--- src/App.js
+++ src/App.js
@@ -7,7 +7,6 @@ function Welcome() {
 
   return (
     <div>
-      
       <Home />
       
       
---
+++ src/Component/Pages/Home.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Home from '../../Home';
+
+
+const Home = () => {
+    return (
+        <div className="home">
+            <Header />
+            <Partners />
+            <Network />
+            <Team />
+            <Footer />
+            
+        </div>
+    );
+}
+
+export default Home;
