f7c1d85add4dcf42b5d2bbf3071c42a06ccdd829
FaridaAdamuMagaji <faridaadamumagaji@gmail.com>
2025-02-14 15:00:38 -0800

    added homepage

--- src/Component/Teamchild.js
+++ src/Component/Teamchild.js
@@ -9,18 +9,21 @@ const TeamList = (props) => {
                 <p className="our">Our Talents</p>
                 <p className="dolor">Lorem ipsum, dolor sit amet consectetur<span className="elicit">adipisicing elit.</span><span className="nemo">Suscipit nemo hic quos, ab,</span></p>
             </div>
-            {teams.map((team) => (
-                <div className="design" key={team.id}>
-                    <div className="team">
-                        <div className="designer">
-                        <img className="designer" src={team.image} alt={team.title}></img>
+            <div className="developer">
+                {teams.map((team) => (
+                    <div key={team.id}>
+                        <div className="team">
+                            <div className="designer">
+                                <img className="designer" src={team.image} alt={team.title}></img>
+                            </div>
+                            <p className="names">{team.names}</p>
+                            <p className="titles">{team.title}</p>
                         </div>
-                        <p className="names">{team.names}</p>
-                        <p className="titles">{team.title}</p>
                     </div>
-                </div>
                 
-            ))}
+                ))}
+            </div>
+            
             <div className="last">
                 <button className="learns">
                     <a className="mores" href="learn more">Learn More</a>
